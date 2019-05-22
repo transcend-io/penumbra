d0f00b0215879973bc6d29b3819234d19f0d8926
Ben Brook <benbrook@me.com>
2019-05-21 21:55:05 -0700

    Add modules.d.ts

---
+++ modules.d.ts
@@ -0,0 +1,25 @@
+declare module 'crypto-browserify' {
+  import { CipherGCMTypes,  CipherGCMOptions, DecipherGCM } from 'crypto';
+  export function createDecipheriv(
+    algorithm: CipherGCMTypes,
+    key: string | Buffer | NodeJS.TypedArray | DataView,
+    iv: string | Buffer | NodeJS.TypedArray | DataView,
+    options?: CipherGCMOptions,
+  ): DecipherGCM;
+}
+
+
+declare module 'streamsaver' {
+  export function createWriteStream(
+    filename: string,
+    size?: number,
+  ): WritableStream;
+}
+
+
+declare module 'typedarray-to-buffer' {
+  type TypedArray = Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array | Uint8ClampedArray | Float32Array | Float64Array;
+  export default function toBuffer(
+    arr: TypedArray,
+  ): Buffer;
+}
