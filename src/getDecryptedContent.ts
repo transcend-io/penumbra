cc8c320245fd76c2df2434b4b847edd0ec9bd8ab
Eli Grey <~@eligrey.com>
2019-07-09 14:03:25 -0700

    Make text type checker case insensitive

--- src/getDecryptedContent.ts
+++ src/getDecryptedContent.ts
@@ -4,7 +4,7 @@ import { RemoteResource } from './types';
 import { getMediaSrcFromRS, getTextFromRS } from './utils';
 
 const MEDIA_TYPES = ['image', 'video', 'audio'];
-const TEXT_TYPES = /^\s*(?:text\/\S*|application\/(?:xml|json)|\S*\/\S*\+xml|\S*\/\S*\+json)\s*(?:$|;)/;
+const TEXT_TYPES = /^\s*(?:text\/\S*|application\/(?:xml|json)|\S*\/\S*\+xml|\S*\/\S*\+json)\s*(?:$|;)/i;
 
 /**
  * Get the contents of an encrypted file
