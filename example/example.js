2173a0f88ffbaa1c9a7e5e3319a7637ad86b5b22
Ben Brook <bencmbrook@users.noreply.github.com>
2019-07-10 12:45:31 -0700

    Add snyk badge and remove unused deps from /example (#34)

--- example/example.js
+++ example/example.js
@@ -6,12 +6,12 @@
  */
 
 import * as Comlink from 'comlink';
-import * as JSZip from 'jszip';
+import * as JSZip from 'jszip'; // TODO switch to @transcend-io/conflux
 import { saveAs } from 'file-saver';
 import { downloadEncryptedFile, getDecryptedContent } from '../build/src/index';
 import * as files from './files';
 
-const USE_SERVICE_WORKER = true;
+const USE_SERVICE_WORKER = false;
 const app = document.getElementById('app');
 
 const S3_URL = 'https://s3-us-west-2.amazonaws.com/bencmbrook/';
