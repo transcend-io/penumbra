5621b9f509b80b116589e6e3447d1190d24fcc05
eligrey <~@eligrey.com>
2019-07-26 18:05:05 -0700

    Remove airtap tests from Travis

--- src/events.d.ts
+++ src/events.d.ts
@@ -1,7 +1,7 @@
 /* tslint:disable completed-docs */
 
 // penumbra
-import { ProgressEmit, PenumbraReady } from './types';
+import { PenumbraReady, ProgressEmit } from './types';
 
 declare global {
   /**
