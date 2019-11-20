d91b38aa6635b8eeb6765db665e9a3153fa7fe58
michaelfarrell76 <mkfrl09@gmail.com>
2019-11-20 03:30:38 -0800

    getPenumbra had circular

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,13 @@
 
 // exports
 import penumbra from './API';
-import getPenumbra from './getPenumbra';
 import MOCK_API from './mock';
 import './transferHandlers/penumbra-events';
 import { PenumbraAPI } from './types';
 
 export * from './types';
 
-export { penumbra, getPenumbra, MOCK_API };
+export { penumbra, MOCK_API };
 
 import intoStream = require('into-stream');
 
@@ -35,3 +34,29 @@ self.intoStream = intoStream;
 self.penumbra = penumbra;
 
 self.dispatchEvent(new CustomEvent('penumbra-ready', { detail: { penumbra } }));
+
+/**
+ * Get the penumbra from window and ensure it is defined
+ *
+ * @param mock - Mock penumbra when environment variable `PENUMBRA_MOCK` is true
+ * @returns The penumbra API object
+ */
+export function getPenumbra(
+  mock = process.env.PENUMBRA_MOCK === 'true',
+): PenumbraAPI {
+  // Mock the API
+  if (mock) {
+    return MOCK_API;
+  }
+
+  // Pull off of the window
+  const { penumbra: currentPenumbra } = self; // eslint-disable-line no-restricted-globals
+
+  if (!currentPenumbra) {
+    throw new Error(
+      'Penumbra does not exist on self! Ensure it is loaded in the index.html file',
+    );
+  }
+
+  return currentPenumbra;
+}
