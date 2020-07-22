63466d084c9e96b7f6e5a221d6a3cbb32df68edb
eligrey <~@eligrey.com>
2020-07-22 04:04:30 -0700

    Keep track of penumbra job completion in demo for benchmarking purposes
    Also locally host Earth.jpg.enc to remove network latency variance from benchmark

--- example/index.js
+++ example/index.js
@@ -1,43 +1,44 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable require-jsdoc */
 const files = [
+  // {
+  //   url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/NYT.txt',
+  //   filePrefix: 'NYT',
+  //   mimetype: 'text/plain',
+  // },
+  // {
+  //   url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/NYT.txt.enc',
+  //   filePrefix: 'NYT',
+  //   mimetype: 'text/plain',
+  //   decryptionOptions: {
+  //     key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
+  //     iv: '6lNU+2vxJw6SFgse',
+  //     authTag: 'gadZhS1QozjEmfmHLblzbg==',
+  //   },
+  // },
+  // {
+  //   url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/tortoise.jpg.enc',
+  //   filePrefix: 'tortoise',
+  //   mimetype: 'image/jpeg',
+  //   decryptionOptions: {
+  //     key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
+  //     iv: '6lNU+2vxJw6SFgse',
+  //     authTag: 'ELry8dZ3djg8BRB+7TyXZA==',
+  //   },
+  // },
+  // {
+  //   url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/water.png.enc',
+  //   filePrefix: 'water',
+  //   mimetype: 'image/png',
+  //   decryptionOptions: {
+  //     key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
+  //     iv: '6lNU+2vxJw6SFgse',
+  //     authTag: 'O7OO9ITvYg5EgdNdIVwbVw==',
+  //   },
+  // },
   {
-    url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/NYT.txt',
-    filePrefix: 'NYT',
-    mimetype: 'text/plain',
-  },
-  {
-    url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/NYT.txt.enc',
-    filePrefix: 'NYT',
-    mimetype: 'text/plain',
-    decryptionOptions: {
-      key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
-      iv: '6lNU+2vxJw6SFgse',
-      authTag: 'gadZhS1QozjEmfmHLblzbg==',
-    },
-  },
-  {
-    url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/tortoise.jpg.enc',
-    filePrefix: 'tortoise',
-    mimetype: 'image/jpeg',
-    decryptionOptions: {
-      key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
-      iv: '6lNU+2vxJw6SFgse',
-      authTag: 'ELry8dZ3djg8BRB+7TyXZA==',
-    },
-  },
-  {
-    url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/water.png.enc',
-    filePrefix: 'water',
-    mimetype: 'image/png',
-    decryptionOptions: {
-      key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
-      iv: '6lNU+2vxJw6SFgse',
-      authTag: 'O7OO9ITvYg5EgdNdIVwbVw==',
-    },
-  },
-  {
-    url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/Earth.jpg.enc',
+    // url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/Earth.jpg.enc',
+    url: '/Earth.jpg.enc',
     filePrefix: 'Earth',
     mimetype: 'image/jpeg',
     decryptionOptions: {
@@ -46,56 +47,56 @@ const files = [
       authTag: 'v86dSoD5yu4iaDZdPYWTDA==',
     },
   },
-  {
-    url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/turtl.gif.enc',
-    filePrefix: 'turtl',
-    mimetype: 'image/gif',
-    decryptionOptions: {
-      key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
-      iv: '6lNU+2vxJw6SFgse',
-      authTag: '6Q+67PfEE/4/qp9CPgR4Hg==',
-    },
-  },
-  {
-    url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/africa.topo.json.enc',
-    filePrefix: 'africa.topo',
-    mimetype: 'application/json',
-    decryptionOptions: {
-      key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
-      iv: '6lNU+2vxJw6SFgse',
-      authTag: 'jw6UL2SQjOr0IwgF6TzxFA==',
-    },
-  },
-  {
-    url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/k.webm.enc',
-    filePrefix: 'k',
-    mimetype: 'video/webm',
-    decryptionOptions: {
-      key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
-      iv: '6lNU+2vxJw6SFgse',
-      authTag: 'K3MVZrK2/6+n8/p/74mXkQ==',
-    },
-  },
-  {
-    url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/river.jpg.enc',
-    filePrefix: 'river',
-    mimetype: 'image/jpeg',
-    decryptionOptions: {
-      key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
-      iv: '6lNU+2vxJw6SFgse',
-      authTag: 'xdbXMuw8Q+uSnOYEXtMr4A==',
-    },
-  },
-  {
-    url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/patreon.mp4.enc',
-    filePrefix: 'patreon',
-    mimetype: 'video/mp4',
-    decryptionOptions: {
-      key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
-      iv: '6lNU+2vxJw6SFgse',
-      authTag: 'Mqgmwj9IvLEeBjhwrkYpBg==',
-    },
-  },
+  // {
+  //   url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/turtl.gif.enc',
+  //   filePrefix: 'turtl',
+  //   mimetype: 'image/gif',
+  //   decryptionOptions: {
+  //     key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
+  //     iv: '6lNU+2vxJw6SFgse',
+  //     authTag: '6Q+67PfEE/4/qp9CPgR4Hg==',
+  //   },
+  // },
+  // {
+  //   url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/africa.topo.json.enc',
+  //   filePrefix: 'africa.topo',
+  //   mimetype: 'application/json',
+  //   decryptionOptions: {
+  //     key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
+  //     iv: '6lNU+2vxJw6SFgse',
+  //     authTag: 'jw6UL2SQjOr0IwgF6TzxFA==',
+  //   },
+  // },
+  // {
+  //   url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/k.webm.enc',
+  //   filePrefix: 'k',
+  //   mimetype: 'video/webm',
+  //   decryptionOptions: {
+  //     key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
+  //     iv: '6lNU+2vxJw6SFgse',
+  //     authTag: 'K3MVZrK2/6+n8/p/74mXkQ==',
+  //   },
+  // },
+  // {
+  //   url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/river.jpg.enc',
+  //   filePrefix: 'river',
+  //   mimetype: 'image/jpeg',
+  //   decryptionOptions: {
+  //     key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
+  //     iv: '6lNU+2vxJw6SFgse',
+  //     authTag: 'xdbXMuw8Q+uSnOYEXtMr4A==',
+  //   },
+  // },
+  // {
+  //   url: 'https://s3-us-west-2.amazonaws.com/bencmbrook/patreon.mp4.enc',
+  //   filePrefix: 'patreon',
+  //   mimetype: 'video/mp4',
+  //   decryptionOptions: {
+  //     key: 'vScyqmJKqGl73mJkuwm/zPBQk0wct9eQ5wPE8laGcWM=',
+  //     iv: '6lNU+2vxJw6SFgse',
+  //     authTag: 'Mqgmwj9IvLEeBjhwrkYpBg==',
+  //   },
+  // },
 ];
 
 // Prepopulate the table with the file metadata, before they've downloaded
