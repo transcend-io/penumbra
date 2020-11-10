e80602687153275938715ea958efa7b896441903
Eli Grey <elijah@transcend.io>
2020-11-10 14:21:54 -0800

    Decode base64 without Node globals (#177)

--- src/utils/toBuff.ts
+++ src/utils/toBuff.ts
@@ -1,14 +1,17 @@
 import toBuffer from 'typedarray-to-buffer';
 
 /**
- * Convert to buffer
+ * Convert to NodeJS Buffer. The input is simply returned if
+ * it is already a NodeJS Buffer.
  *
  * @param i - The input buffer or string
  * @returns Enforced as buffer
  */
 export default (i: Buffer | string | ArrayBufferView): Buffer =>
-  ArrayBuffer.isView(i)
-    ? toBuffer(i as NodeJS.TypedArray)
-    : typeof i === 'string'
-    ? Buffer.from(i, 'base64')
-    : i;
+  toBuffer(
+    ArrayBuffer.isView(i)
+      ? // input is a typed array
+        ((i as unknown) as NodeJS.TypedArray)
+      : // input is a base64 string
+        Uint8Array.from(atob(i), (c) => c.charCodeAt(0)),
+  );
