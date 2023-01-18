2a7b0b6c0b3c72e934719ddfe3aa6e27e9a2fa8d
bencmbrook <ben@transcend.io>
2023-01-17 19:57:41 -0800

    log level -> >=warn in CI

--- karma.global.js
+++ karma.global.js
@@ -65,7 +65,7 @@ module.exports = (config) => ({
 
   // level of logging
   // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-  logLevel: config.LOG_INFO,
+  logLevel: process.env.CI ? config.LOG_WARN : config.LOG_INFO,
 
   // enable / disable watching file and executing tests whenever any file changes
   autoWatch: false,
