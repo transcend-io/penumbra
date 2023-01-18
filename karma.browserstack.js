8a9844acc9b28a1de67f4784378de195c223967b
bencmbrook <ben@transcend.io>
2023-01-17 18:46:57 -0800

    More stable browserstack versions

--- karma.browserstack.js
+++ karma.browserstack.js
@@ -1,41 +1,41 @@
 // Karma configuration
-const { short } = require('git-rev-sync');
+// const { short } = require('git-rev-sync');
 const getGlobalConfig = require('./karma.global');
+const packageJson = require('./package.json');
 
 module.exports = (config) => {
+  /**
+   * @see https://www.browserstack.com/automate/capabilities
+   */
   const customLaunchers = {
-    bs_chrome_pc: {
+    bs_chrome_mac: {
       base: 'BrowserStack',
       browser: 'Chrome',
-      browser_version: '85',
-      os: 'Windows',
-      os_version: '10',
-      device: null,
-      real_mobile: null,
+      browser_version: '104.0',
+      os: 'OS X',
+      os_version: 'Big Sur',
+    },
+    bs_safari_mac: {
+      base: 'BrowserStack',
+      browser: 'Safari',
+      browser_version: '15.6',
+      os: 'OS X',
+      os_version: 'Monterey',
     },
     bs_firefox_pc: {
       base: 'BrowserStack',
       browser: 'Firefox',
-      browser_version: '80',
+      browser_version: '102.0',
       os: 'Windows',
       os_version: '10',
-      device: null,
-      real_mobile: null,
     },
-    /**
-     * TODO: https://github.com/transcend-io/penumbra/issues/164
-     * Uncomment this entry once BrowserStack supports Safari 14.
-     * In the meantime Safari can be tested locally through `yarn start:demo`.
-     */
-    // bs_safari_mac: {
-    //   base: 'BrowserStack',
-    //   browser: 'Safari',
-    //   browser_version: '14',
-    //   os: 'OS X',
-    //   os_version: 'Catalina',
-    //   device: null,
-    //   real_mobile: null,
-    // },
+    bs_edge_pc: {
+      base: 'BrowserStack',
+      browser: 'Edge',
+      browser_version: '108.0',
+      os: 'Windows',
+      os_version: '11',
+    },
   };
 
   const globalConfig = getGlobalConfig(config);
@@ -53,7 +53,11 @@ module.exports = (config) => {
       username: 'benjaminbrook3',
       project: 'Penumbra',
       video: false,
-      build: process.env.TRAVIS_BUILD_NUMBER || short(), // process.env.CIRCLE_BUILD_NUM
+      build: `Karma - penumbra@${packageJson.version}${
+        process.env.GITHUB_RUN_ID
+          ? ` - GitHub Run ID: ${process.env.GITHUB_RUN_ID}`
+          : ''
+      }`,
     },
 
     // define browsers
