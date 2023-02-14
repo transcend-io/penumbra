9afa210ad0add99255490b0a53eb7ad45543bc2a
bencmbrook <ben@transcend.io>
2023-02-14 11:03:36 -0800

    Re-enable self-signed SSL cert

--- karma.browserstack.js
+++ karma.browserstack.js
@@ -22,14 +22,13 @@ module.exports = (config) => {
       os: 'OS X',
       os_version: 'Monterey',
     },
-    // TODO: https://github.com/transcend-io/penumbra/issues/249
-    // bs_firefox_pc: {
-    //   base: 'BrowserStack',
-    //   browser: 'Firefox',
-    //   browser_version: '102.0',
-    //   os: 'Windows',
-    //   os_version: '10',
-    // },
+    bs_firefox_pc: {
+      base: 'BrowserStack',
+      browser: 'Firefox',
+      browser_version: '102.0',
+      os: 'Windows',
+      os_version: '10',
+    },
     bs_edge_pc: {
       base: 'BrowserStack',
       browser: 'Edge',
@@ -57,10 +56,9 @@ module.exports = (config) => {
       build: `penumbra@${packageJson.version} - ${short()} - ${
         process.env.GITHUB_RUN_ID ? `CI: ${process.env.GITHUB_RUN_ID}` : 'Local'
       }`,
-      // TODO - https://github.com/transcend-io/penumbra/issues/249
-      // // A secure context must be used for `crypto.subtle` to be defined
-      // acceptInsecureCerts: 'true',
-      // acceptSslCerts: 'true',
+      // A secure context must be used for `crypto.subtle` to be defined
+      acceptInsecureCerts: 'true',
+      acceptSslCerts: 'true',
     },
 
     // define browsers
