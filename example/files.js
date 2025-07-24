1b96122a31b5a74dc3d7460ea5c053efbb2b104f
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-07-24 12:38:26 -0400

    Fix example remote

--- example/files.js
+++ example/files.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-unresolved
 import fixtures from './files/fixtures.json' with { type: 'json' };
 
-window.files = fixtures;
+window.files = fixtures.filter((f) => f.filePrefix !== 'big');
