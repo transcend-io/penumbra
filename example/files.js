61e92828d9482d031aa9c98402263c3845e65309
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-08-03 23:07:15 -0400

    rm demos for now

--- example/files.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import/no-unresolved
-import fixtures from '../fixtures/files/fixtures.json' with { type: 'json' };
-
-window.files = fixtures.filter((f) => f.filePrefix !== 'big');
