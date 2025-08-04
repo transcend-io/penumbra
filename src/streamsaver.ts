5cbd8a8252a423fcbaee5e737f9ea45250471dd3
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-08-03 21:42:45 -0400

    fix streamsaver endpoint

--- src/streamsaver.ts
+++ src/streamsaver.ts
@@ -2,6 +2,7 @@ import streamSaver from 'streamsaver/StreamSaver.js';
 import { settings } from './settings';
 
 streamSaver.mitm =
-  settings.streamsaverEndpoint || 'https://streaming.transcend.io/mitm.html';
+  settings.streamsaverEndpoint ||
+  'https://streaming.transcend.io/endpoint.html';
 
 export { streamSaver };
