bd1372d4c6b7a072b9921e73f1e4d9b04f87a58a
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-08-06 17:10:19 -0400

    regex vuln fix

--- src/utils/is-viewable-text.ts
+++ src/utils/is-viewable-text.ts
@@ -1,5 +1,5 @@
 const TEXT_TYPES =
-  /^\s*(?:text\/\S*|application\/(?:xml|json)|\S*\/\S*\+xml|\S*\/\S*\+json)\s*(?:$|;)/i;
+  /^\s*(?:text\/[^/\s]+|application\/(?:xml|json)|[^/\s]+\/[^/\s]+\+(?:xml|json))\s*(?:$|;)/i;
 
 // const codecTester = document.createElementNS(
 //   'http://www.w3.org/1999/xhtml',
@@ -12,6 +12,9 @@ const TEXT_TYPES =
  * @returns 'probably', 'maybe', or '' depending on if mime type can be displayed
  */
 export default function isViewableText(mimetype: string): boolean {
+  if (typeof mimetype !== 'string') {
+    return false;
+  }
   const type = mimetype.split('/')[0].trim().toLowerCase();
   return type === 'text' || TEXT_TYPES.test(mimetype);
 }
