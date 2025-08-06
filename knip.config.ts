b2f16f127d80161f15fb250813c8f64c8e475f4e
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-08-06 17:36:34 -0400

    depcheck->knip

--- knip.config.ts
+++ knip.config.ts
@@ -2,14 +2,18 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
   entry: [
-    'src/index.ts',
-    'src/worker.penumbra.js',
+    'src/index.ts!',
+    'src/api.ts!', // knip needs a little help getting past the actual entrypoint
+    'src/worker.penumbra.js!',
+    'src/worker.ts!', // knip needs a little help getting past the actual entrypoint
     'tests/*.test.ts',
     'web-test-runner.config.js',
   ],
+  project: ['src/**', 'fixtures/**', 'tests/**'],
   ignore: [
     // Vendored third-party library has some unused exports
     'src/utils/brand.ts',
+    'src/**/*.d.ts',
   ],
   ignoreDependencies: ['@web/test-runner-playwright'],
 };
