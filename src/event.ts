ca173e752c344734aff2d5755b076b6ed2d245a3
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-08-11 16:49:37 -0400

    nit: fix CI and simplify events declaration

--- src/event.ts
+++ src/event.ts
@@ -1,1 +1,21 @@
+import type {
+  JobCompletionEmit,
+  PenumbraErrorEmit,
+  ProgressEmit,
+} from './types.js';
+
+declare global {
+  /**
+   * Mapping from event listeners to their underlying types
+   */
+  interface WindowEventMap {
+    /** Emit during Penumbra downloads */
+    'penumbra-progress': ProgressEmit;
+    /** Emit upon the completion of an Penumbra encryption job */
+    'penumbra-complete': JobCompletionEmit;
+    /** Emit whenever the Penumbra worker encounters any errors */
+    'penumbra-error': PenumbraErrorEmit;
+  }
+}
+
 export const PenumbraEvent = CustomEvent;
